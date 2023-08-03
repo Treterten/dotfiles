c50deade4819013c51e395a432985c7c163e7e39
Tre Moore <mitch3moore@gmail.com>
2023-08-02 22:13:52 -0700

    Converted to proper files

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,1 +1,13 @@
-/home/trex/.config/polybar/launch.sh
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Set colors according to those defined by wal
+source "${HOME}/.cache/wal/colors.sh"
+background=$color0
+foreground=$color7
+tertiary=$color2
+fourth=$color4
+
+polybar --reload &
